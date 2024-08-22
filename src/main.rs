03adf4874b119fbe88fd0ab82376fdf7d94e6ea6
ajayspatil7 <aspatil99451@gmail.com>
2024-08-22 18:50:48 +0530

    modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,5 @@
-use std::io;
-
-// Some shitty proj with what I've learnt till now :)
 
 fn main() {
-    let input: i32 = input_age();
-    validate_age(input);
+    println!("Hello, world");
 }
 
-fn validate_age(input: i32) {
-    if (input == 20 || input >= 20) && input < 25 {
-        println!(
-            "Your age is {} and you are not a TEEN anymore you're an early adult :)",
-            input
-        );
-    } else if input >= 25 && input < 60 {
-        println!("Your age is {} and you are a fully mature Adult!", input);
-    } else if input >= 60 && input <= 110 {
-        println!("You are a senior citizen! Great")
-    } else {
-        println!("Invalid age!");
-    }
-}
-
-fn input_age() -> i32 {
-    // Let's take an input
-    let mut input: String = String::new();
-    io::stdin().read_line(&mut input).expect("Couldn't read input");
-
-    // Let's typecast the input to int
-    let input: i32 = input.trim().parse().unwrap();
-
-    // Let's return back the value
-    return input;
-}
